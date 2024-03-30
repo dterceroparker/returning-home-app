49e0a87e69778c88ed979836490547a02e98d77a
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-03-30 10:49:17 -0400

    HTML structure update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # returning-home-app
+
+https://github.com/adam-p/markdown-here/wiki/
+<!-- - **Screenshot:** A screenshot of your game.
+
+- **Your game’s title**: A description of your game. Background info about why you chose the game is a nice touch.
+
+- **Getting Started**: Include a link to your deployed game and any instructions you deem important.
+
+- **Attributions**: Include links to any external resources (such as libraries or assets) you used to develop your application.
+
+- **Technologies Used**: List of the technologies used, for example: JavaScript, HTML, CSS, etc.
+
+- **Next Steps**: Planned future enhancements (icebox items).
+
+- Your **`README.md`** file must be grammatically correct and free of spelling errors. -->
---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+//light dark mode
+const dayNightBtn = document.getElementById('day-night-theme')
+
+dayNightBtn.addEventListener('click', () => {
+  document.body.classList.toggle('night-theme')
+})
+
+
+
+// - **Render a game in the browser**. You may not use Canvas or jQuery.
+// - **Include win/loss logic and render win/loss messages in HTML.**
+// - Include separate HTML, CSS, JavaScript, and JavaScript data files organized in an appropriate directory structure.
+// - Game must have reset functionality that does not reload the page in the browser.
+// - Include functionality specific to your game as specified in square brackets in the Recommended Games section below, or as discussed with your instructor if building a game not on the Recommended Games list.
