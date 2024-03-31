6eb56e633d6b2a50b787d5585d169785c7ce137c
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-03-30 23:06:22 -0400

    quotes.js 20 quotes addition

--- data/quotes.js
+++ data/quotes.js
@@ -1,3 +1,36 @@
+const comparisonQuotes = [
+  {
+    quote: "Then even if one thousand people would say that it is useless, that it does not make sense to practice mindful breathing, you would still smile. Because you know from your own experience that mindful breathing helps you to be fresh, to be peaceful, to be happy. So the opinions of other people cannot make you abandon your conviction, your belief. ~ Thích Nhất Hạnh"
+  },
+  {
+    quote: "You know that the notion of beauty differs with every person. Something can be very beautiful to someone but not beautiful to other people. The notions of beauty, or ugliness--they are notions--and you are not affected by their opinions. ~ Thích Nhất Hạnh"
+  },
+  {
+    quote: "You are a flower in the garden of humanity. You are not something less than a flower. ~ Thích Nhất Hạnh"
+  },
+  {
+    quote: "Many millions of people misunderstood us and yet we still continued with our path, because we had belief in our values. I think if you continue to practice like that, you will be solid as a mountain. You will not be assaulted by any kind of opinions. ~ Thích Nhất Hạnh"
+  },
+  {
+    quote: "People who are happy use themselves for internal evaluation. It's not that they don't notice upward comparisons, but they don't let that affect their self-esteem, and they stay focused on their own improvement. A happy runner compares himself to his last run, not to others who are faster. ~ Sonja Lyubormirsky"
+  },
+  {
+    quote: "Recognise that comparing yourself is a habit you have developed over your lifetime, and it isn't your fult. When we identify something as a habit and are honest about its impact on our self-esteem and mental health, we are in a much better position to change it. Start by noting down how many times you compare yourself throughout the day; you may be surprised at the number. ~ Psychology Today"
+  },
+  {
+    quote: "Do you know why you compare yourself to others (apart from growing up in a society and culture that invites it)? We compare ourselves to others for many reasons, and knowing your reason will be helpful when managing it. Each time you compare yourself to someone else, note what the comparison was (e.g. lifestyle, body image, career or study, appearance, etc). Over time, you will see a theme that will help you identify why social comparison is habitual for you. ~ Psychology Today"
+  },
+  {
+    quote: "Comparing yourself to others is not only a habit but a habit that has become automatic. You probably don't realize you are doing it. Thus, one way to break the comparison habit is to bring awareness every time you do it: 'Ah, here is an unhelpful comparison thought!' ~ Psychology Today"
+  },
+  {
+    quote: "Once you know when you are comparing, you can choose what you do with it. You can either take it on board, believe it to be true, or not. After all, it is just a thought like any other, and we can choose what thoughts we buy into based on whether they are helpful to us and help us move toward the life we want to live. ~ Psychology Today"
+  },
+  {
+    quote: "It makes you consider shoes you don't like, but when that shoe doesn't fit, wear what you will, despite how they feel, comparison kills, the grass was fine, until it looked greener on the other side, now you're believing that you fell behind, but why try to match what should be one of a kind, you're one of a kind. ~ Jonathan McReynolds"
+  }
+]
+
 const impostorSyndromeQuotes = [
   {
     quote: "Faced with responsibility or prestige, we quickly become convinced that we are simply impostors, like an actor in the role of a pilot, wearing the uniform and making sunny cabin announcements while incapable of even starting the engines. It can feel easier simply not to try. ~ School of Life"
