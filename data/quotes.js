d1c31e40f2c2a9282e037959f24db3afa06248af
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-03-30 20:00:47 -0400

    JS quotes addition

---
+++ data/quotes.js
@@ -0,0 +1,32 @@
+const impostorSyndromeQuotes = [
+  {
+    quote: "Faced with responsibility or prestige, we quickly become convinced that we are simply impostors, like an actor in the role of a pilot, wearing the uniform and making sunny cabin announcements while incapable of even starting the engines. It can feel easier simply not to try. ~ School of Life"
+  },
+  {
+    quote: "The root cause of the impostor syndrome is a hugely unhelpful picture of what other people are really like. We feel like impostors not because we are uniquely flawed, but because we fail to imagine how deeply flawed everyone else must necessarily also be beneath a more or less polished surface. ~ School of Life"
+  },
+  {
+    quote: "We know ourselves from the inside, but others only from the outside. We're constantly aware of all our anxieties, doubts and idiocies from within. Yet all we know of others is what they happen to do and tell us, a far narrower, and more edited source of information. ~ School of Life"
+  },
+  {
+    quote: "The solution to the impostor syndrome lies in making a crucial leap of faith, the leap that others' minds work in basically much the same ways as ours do. ~ School of Life"
+  },
+  {
+    quote: "Everyone must be as anxious, uncertain and wayward as we are. ~ School of Life" 
+  },
+  {
+    quote: "It's a leap of faith because we just have to accept that the majority of what we feel and are, especially the more shameful, unmentionable sides, will have a corollary in each and every one of us. ~ School of Life"
+  },
+  {
+    quote: 'Making a leap of faith around what other people are really like helps us to humanize the world. ~ School of Life' 
+  }
+  {
+    quote: "It means that whenever we encounter a stranger we're not really encountering a stranger, we're in fact encountering someone who is - in spite of the surface evidence to the contrary - in basic ways very much like us - and that therefore nothing fundamental stands between us and the possibility of responsibility, success and fulfillment. ~ School of Life"
+  },
+  {
+    quote: "If you walk on stilts, you're still walking on your feet. If you sit on the highest throne in the world, you're still sitting on your ass. ~ Michel de Montaigne"
+  },
+  {
+    quote: "Kings and philosophers shit [...]. ~ Michel de Montaigne"
+  }
+]
